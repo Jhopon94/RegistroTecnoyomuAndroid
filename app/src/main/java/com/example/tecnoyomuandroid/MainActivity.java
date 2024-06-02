748a25354b9a5462443af0695db2f5294b191bba
John Hader Medina Pacheco <jhopon94@gmail.com>
2024-06-02 14:54:29 -0500

    Terminada activity admon, y ligada al inicio

--- app/src/main/java/com/example/tecnoyomuandroid/MainActivity.java
+++ app/src/main/java/com/example/tecnoyomuandroid/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.tecnoyomuandroid;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void QueAbrir(View vista){
+        TextView cajitaUsuario = (TextView) findViewById(R.id.cajitaUsuario);
+        String usuario = cajitaUsuario.getText().toString();
+        switch (usuario){
+            case "administrador":
+                AbrirAdmon();
+                break;
+            default:
+                Toast.makeText(this, "Usuario Incorrecto", Toast.LENGTH_SHORT).show();
+                break;
+        }
+    }
+    private void AbrirAdmon(){
+        Intent actividad = new Intent(this, Administrador.class);
+        startActivity(actividad);
     }
 }
