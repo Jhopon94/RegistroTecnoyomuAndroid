3ccc9501978fc354bd9edc6098275326560d444e
John Hader Medina Pacheco <jhopon94@gmail.com>
2024-06-07 03:00:00 -0500

    Agregado Contabilidad solo ingresos

--- app/src/main/java/com/example/tecnoyomuandroid/Contabilidad.java
+++ app/src/main/java/com/example/tecnoyomuandroid/Contabilidad.java
@@ -3,10 +3,16 @@ package com.example.tecnoyomuandroid;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.InputType;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class Contabilidad extends AppCompatActivity {
 
--- app/src/main/java/com/example/tecnoyomuandroid/DatosContables.java
+++ app/src/main/java/com/example/tecnoyomuandroid/DatosContables.java
@@ -2,8 +2,17 @@ package com.example.tecnoyomuandroid;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.text.InputType;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.example.tecnoyomuandroid.Entidades.Equipo;
+
+import java.util.List;
 
 public class DatosContables extends AppCompatActivity {
 
@@ -15,5 +24,40 @@ public class DatosContables extends AppCompatActivity {
 
     public void CerrarSesion(View vista){
         finish();
+    }
+
+
+    public void MostrarDialogoAbono(View vista){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("El total de ingresos histórico es: ");
+
+        // Configurar el EditText para ingresar solo números
+        final EditText etIngresos = new EditText(this);
+        etIngresos.setInputType(InputType.TYPE_CLASS_TEXT);
+        int ingresos = CalcularIngresos();
+        etIngresos.setText("$ " + ingresos);
+        etIngresos.setEnabled(false);
+        builder.setView(etIngresos);
+
+        // Configurar botones del diálogo
+        builder.setPositiveButton("Aceptar", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+        builder.show();
+    }
+
+    private int CalcularIngresos(){
+        ConsultaSQLite consultaEqEntreg = new ConsultaSQLite(this);
+        List<Equipo> listaEquipos = consultaEqEntreg.ConsultarEquiposEntregadosSQLite();
+        int sumatoria = 0;
+        if(!listaEquipos.isEmpty()){
+            for(Equipo equipo : listaEquipos){
+                sumatoria += equipo.getPrecio();
+            }
+        }
+        return sumatoria;
     }
 }
